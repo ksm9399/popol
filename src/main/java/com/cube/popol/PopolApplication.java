7e5fccd635df02a488622bf9f58cd6a2d1874104
ksm <rkdtjd0909@gmail.com>
2026-04-03 18:53:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/cube/popol/PopolApplication.java
@@ -0,0 +1,15 @@
+package com.cube.popol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopolApplication.class, args);
+
+		System.out.println("========== application popol cube started ==========");
+	}
+
+}
---
+++ src/test/java/com/cube/popol/PopolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cube.popol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
